a75230085e450a15d2935118005525f7bab0b1b8
sreekarpatti <122248992+sreekarpatti@users.noreply.github.com>
2024-04-04 17:35:08 -0400

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import Hello from "./Hello.js"
+import express from "express";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-import cors from "cors";
-const app = express()
+
+const app = express();
 app.use(cors());
-CourseRoutes(app);
 app.use(express.json());
-ModuleRoutes(app);
 Lab5(app);
-Hello(app)
+Hello(app);
+CourseRoutes(app);
+ModuleRoutes(app);
 app.listen(4000)
