944388c83d13441d216a490c5b948640806670bd
sreekarpatti <122248992+sreekarpatti@users.noreply.github.com>
2024-04-04 17:41:37 -0400

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-import cors from "cors";
 
 const app = express()
 app.use(cors());
